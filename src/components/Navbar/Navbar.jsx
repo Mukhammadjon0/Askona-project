7ad5b14292a13b73a8e0d4f9db1ec34c052d9c0f
Mukhammadjon0 <zokirjonovmuhammadjon0@gmail.com>
2023-02-06 13:49:39 +0500

    navbar-done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,38 +11,33 @@ import { CgShoppingCart } from 'react-icons/cg'
 import { HiBars3BottomLeft } from 'react-icons/hi2'
 import { FaPercentage } from 'react-icons/fa'
 import DioGramma from '../../assets/image/diogramma.png'
+import { NavLink } from 'react-router-dom'
 function Navbar() {
-
-  const submitHandler = e => {
-    e.preventDefault()
-    alert('form ishladi')
-  }
-
   return (
     <div className=''>
-      <div className="w-[1920px] m-auto bg-navTopbBg flex ">
-        <img className='w-10/12 m-auto ' src={Topadvenses} alt="" />
-        <AiFillCloseCircle className='text-white z-1  pt-5 text-5xl text-center align-center' />
-      </div>
-      <div className='w-[1920px] items-center py-5'>
-        <div className='w-[90%] m-auto flex items-center'>
-          <img className='w-[10%] m-auto' src={logoAksona} alt="" />
 
-          <li className='w-28 m-auto'>
-            <a className='flex items-center font-normal text-sm  leading-4 ' href="#!">Минск <BsChevronDown className='text-navTopbBg pl-2 text-3xl' /></a>
+      <div className="flex w-full items-center bg-[#02B9BF]">
+        <img className='w-[80%] m-auto bg-[#00B6C9]' src={Topadvenses} alt="" />
+        <button>
+          <AiFillCloseCircle className='text-white mr-7' />
+        </button>
+      </div>
+
+
+      <div className='w-full items-center py-5 px-20'>
+        <div className='w-full m-auto flex items-center'>
+          <img className='w-[148px] m-auto' src={logoAksona} alt="" />
+          <li className='w-28 m-auto list-none'>
+            <NavLink className='flex items-center font-normal text-sm leading-4' href="#!">Минск <BsChevronDown className='text-navTopbBg pl-2 text-3xl' /></NavLink>
           </li>
           <p className='w-[10%] m-auto text-start flex items-center text-gray-400 text-sm'>Магазины: <span className='font-bold text-black pl-2'>30</span></p>
-
-
-          <form className='rounded py-2 px-1  flex items-center w-96 border border-solid border-FormColor ' action="" onSubmit={submitHandler}>
-            <BiSearchAlt2 className='text-navTopbBg text-lg font-bold' />
+          <form className='rounded py-2 px-1  flex items-center w-96 border border-solid border-FormColor ' action="">
+            <BiSearchAlt2 className='text-[#00B6C9] text-lg font-bold' />
             <input type="text" placeholder='Поиск по товарам...' className="outline-none " />
           </form>
           <button className=' rounded w-[15%] m-auto  flex items-center border border-solid border-FormColor py-2 px-1 pl-5 text-center'><TbPhoneCall className='text-gray-400  pr-2 text-2xl' /> Перезвоните мне</button>
-
           <p className=' width-[5%] m-auto text-black text-lg flex'>7176</p>
-          <span className='w-[5%] m-auto items-center text-navTopbBg text-center'>показать <br />номера</span>
-
+          <span className='w-[5%] m-auto items-center text-[#00B6C9] text-center'>показать <br />номера</span>
           <div className=' w-[10%] m-auto flex justify-between text-2xl  text-black '>
             <BiUser />
             <img src={DioGramma} alt="" />
@@ -50,42 +45,40 @@ function Navbar() {
             <CgShoppingCart />
           </div>
         </div>
-
-
       </div>
 
-      <div className='w-[1920px]  m-auto  '>
-        <div className="w-[90%] m-auto flex items-center">
+      <div className='w-full px-20 border-b-[1px] border-gray-200'>
+        <div className="flex items-center">
 
-          <button className=' w-[15%] m-auto border border-solid  bg-navTopbBg pl-16 py-3 flex items-center text-white font-bold'><HiBars3BottomLeft />  Все товары </button>
+          <button className=' w-[15%] m-auto border border-solid  bg-[#00B6C9] pl-16 py-3 flex items-center text-white font-bold'><HiBars3BottomLeft />  Все товары </button>
           <ul className='w-[70%] m-auto flex justify-between pl-5'>
             <li>
-              <a className='text-black text-lg font-medium leader-5 ' href="#!">Матрасы</a>
+              <NavLink className='text-black text-lg font-medium leader-5 ' href="#!">Матрасы</NavLink>
             </li>
             <li>
-              <a className='text-black text-lg font-medium leader-5 ' href="#!">Подушки</a>
+              <NavLink className='text-black text-lg font-medium leader-5 ' href="#!">Подушки</NavLink>
             </li>
             <li>
-              <a className='text-black text-lg font-medium leader-5 ' href="#!">Кровати</a>
+              <NavLink className='text-black text-lg font-medium leader-5 ' href="#!">Кровати</NavLink>
             </li>
             <li>
-              <a className='text-black text-lg font-medium leader-5 ' href="#!">Одеяла</a>
+              <NavLink className='text-black text-lg font-medium leader-5 ' href="#!">Одеяла</NavLink>
             </li>
             <li>
-              <a className='text-black text-lg font-medium leader-5 ' href="#!">Диваны</a>
+              <NavLink className='text-black text-lg font-medium leader-5 ' href="#!">Диваны</NavLink>
             </li>
             <li>
-              <a className='text-black text-lg font-medium leader-5 ' href="#!">Детские товары</a>
+              <NavLink className='text-black text-lg font-medium leader-5 ' href="#!">Детские товары</NavLink>
             </li>
             <li>
-              <a className='text-black text-lg font-medium leader-5 ' href="#!">Мебель </a>
+              <NavLink className='text-black text-lg font-medium leader-5 ' href="#!">Мебель </NavLink>
             </li>
             <li>
-              <a className='text-black text-lg font-medium leader-5 ' href="#!">Для дома</a>
+              <NavLink className='text-black text-lg font-medium leader-5 ' href="#!">Для дома</NavLink>
             </li>
           </ul>
 
-          <a className='w-[10%] m-auto text-red-400 flex items-center px-8 text-lg' href="#!"> <FaPercentage /> Акции</a>
+          <NavLink className='w-[10%] m-auto text-red-400 flex items-center px-8 text-lg' href="#!"> <FaPercentage /> Акции</NavLink>
         </div>
 
       </div>
